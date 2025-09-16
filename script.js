48afca018626926b83b63843ab3f94c62b0bfca9
Roseira1212 <joao.roseira.ferrari@escola.pr.gov.br>
2025-09-16 12:18:02 -0300

    a

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const sons = {
-    
+    'A' : 'boom.wav',
+    'S' : 'clap.wav',
+    'D' : 'hithav.wav',
+    'F' : 'kick.wav',
+    'G' : 'opennat.wav',
+    'H' : 'ride.wav',
+    'J' : 'snare.wav',
+    'K' : 'tink.wav',    
+}
+// função para criar para criar um div para cada tecla
+const criarDiv = (sons) => {
+div. classList ('key');
+div.textContent = texto;
 }
